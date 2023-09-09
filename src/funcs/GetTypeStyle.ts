be344dd997ded084a8c1d39ac51e4ac4de98d90c
Comparitiko <gabigcy@gmail.com>
2023-09-09 18:35:51 +0200

    Doing types styles

--- src/funcs/GetTypeStyle.ts
+++ src/funcs/GetTypeStyle.ts
@@ -3,6 +3,9 @@ let typeStyle: string
 export function getTypeStyle (type: string) {
   switch (type) {
     case 'grass':
-      typeStyle = 'hola'
+      typeStyle = "border border-green-800 rounded-xl bg-green-500 text-white px-1"
+      return typeStyle
+    default:
+      typeStyle = ""
   }
 }
