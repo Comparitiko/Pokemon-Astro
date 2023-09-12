53f95dbadc0d1df8f362fc9e2305525987b010d8
Gabriel Collado <gabigcy@gmail.com>
2023-09-12 22:46:36 +0200

    Fixed some errors

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,6 @@
 import type { Pokedex } from "../types/API-pokedex"
 import type { Pokemon } from "../types/API-pokemon"
+import type { Type, Types } from "../types/API-pokemon"
 
 export async function getPokemonByName (name:string) {
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
@@ -17,4 +18,16 @@ export async function getPokemonWithLimit(offset:number) {
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=50&offset=${offset}`)
   const limitedPokedex: Pokedex = await res.json()
   return limitedPokedex
+}
+
+export async function getAllTypes () {
+  const res = await fetch(`https://pokeapi.co/api/v2/type/`)
+  const type = await res.json()
+  return type
+}
+
+export async function getTypeData(type:string) {
+  const res = await fetch(`https://pokeapi.co/api/v2/type/${type}`)
+  const typeData: = await res.json()
+  return typeData
 }
